614336a3e7fdcfb6e70e6bd57317629a45704601
Kevin <kevin@kevin-Gateway-Viper-C.(none)>
2015-04-21 13:56:30 -0700

    Combining versions on the desktop and laptop. Not sure exactly what's in this.

--- client/views/environment_view/helpers.js
+++ client/views/environment_view/helpers.js
@@ -1,6 +1,18 @@
-Template.environment_view.rendered = function(){
-    var canvas = $(this.find(".canvas"));
-    this.stage = (this.stage || new PIXI.Stage(0xFFFFFF));
-    this.renderer = (this.renderer || PIXI.autoDetectRenderer(Math.min(canvas.width(), Window.width),Math.min(canvas.height(), Window.height)));
-    canvas.append(this.renderer.view);
-};
+Template.environment_view.helpers({
+    clear_canvas : function(){
+            console.log("Clearing Canvas");
+            if (Session.get('init')){
+                var canvas = pixi_elements.canvas = $(this.find(".canvas"));
+                var stage = pixi_elements.stage = new PIXI.Stage(0xFFFFFF);
+                var renderer = pixi_elements.renderer = PIXI.autoDetectRenderer(Math.min(canvas.width(), Window.width),Math.min(canvas.height(), Window.height));
+                canvas.append(renderer.view);
+                console.log("Canvas Cleared");
+            } else {
+                console.log("No need to clear Canvas");
+                return false;
+            }
+    },
+    draw_soil : function(soil){
+        console.log(soil);
+    }
+});
