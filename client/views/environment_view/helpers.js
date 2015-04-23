9524e65af8a9b86953003f3f684b9b5022554a74
mrunderhill89 <mrunderhill89@gmail.com>
2015-04-23 10:44:24 -0700

    Finally got around to implementing Pixi canvases in templates.
    They're not working, because of an issue with template.find() and .$(),
    but it's almost there.

--- client/views/environment_view/helpers.js
+++ client/views/environment_view/helpers.js
@@ -1,16 +1,29 @@
+function clear_canvas(template){
+
+    /*
+    if (!template.stage){
+        
+        var stage = template.stage = new PIXI.Stage(0xFFFFFF);
+        var renderer = template.renderer = PIXI.autoDetectRenderer(Math.min(canvas.width(), Window.width),Math.min(canvas.height(), Window.height));
+        canvas.append(renderer.view);
+        console.log("Canvas Cleared");
+    } else {
+        console.log("No need to clear Canvas");
+    }
+    */
+}
+
 Template.environment_view.helpers({
     clear_canvas : function(){
-            console.log("Clearing Canvas");
-            if (Session.get('init')){
-                var canvas = pixi_elements.canvas = $(this.find(".canvas"));
-                var stage = pixi_elements.stage = new PIXI.Stage(0xFFFFFF);
-                var renderer = pixi_elements.renderer = PIXI.autoDetectRenderer(Math.min(canvas.width(), Window.width),Math.min(canvas.height(), Window.height));
-                canvas.append(renderer.view);
-                console.log("Canvas Cleared");
-            } else {
-                console.log("No need to clear Canvas");
-                return false;
-            }
+        var template = Template.instance();
+        if (!template.stage){
+            console.log("Create New Stage");
+            var stage = template.stage = new PIXI.Stage(0xFFFFFF);
+            console.log(template.$(".canvas"));
+        } else {
+            
+        }
+        console.log(template);
     },
     draw_soil : function(soil){
         console.log(soil);
